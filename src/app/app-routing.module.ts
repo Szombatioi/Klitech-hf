9d43479193d462f96210be36be5c993d6e44fe9a
Szombati Olivér <o.szombati@gmail.com>
2024-05-02 12:59:40 +0200

    Save - update almost done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HistoryComponent } from './history/history.component';
+import { HistoryComponent } from './components/history/history.component';
 import { DictionaryComponent } from './components/dictionary/dictionary.component';
-import { TranslatorComponent } from './translator/translator.component';
+import { TranslatorComponent } from './components/translator/translator.component';
 
 const routes: Routes = [
   { path: 'translator', component: TranslatorComponent },
-    { path: 'dictionary', component: DictionaryComponent},
-    { path: 'history', component: HistoryComponent}
+  { path: 'dictionary', component: DictionaryComponent},
+  { path: 'history', component: HistoryComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  bootstrap: [TranslatorComponent]
 })
 export class AppRoutingModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppRoutingModule)
   .catch((err) => console.error(err));
