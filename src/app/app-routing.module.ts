914eef5759a7f269a2183c788eb02720d52d6446
Szombati Olivér <o.szombati@gmail.com>
2024-05-02 13:33:43 +0200

    Stable - Redesigned file structure based on Lab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, Router, RouterModule, Routes } from '@angular/router';
 import { HistoryComponent } from './components/history/history.component';
 import { DictionaryComponent } from './components/dictionary/dictionary.component';
 import { TranslatorComponent } from './components/translator/translator.component';
+import { HttpCommunicationService } from './services/http-communication.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-const routes: Routes = [
+const routes: Route[] = [
   { path: 'translator', component: TranslatorComponent },
   { path: 'dictionary', component: DictionaryComponent},
   { path: 'history', component: HistoryComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  declarations: [HistoryComponent, TranslatorComponent, DictionaryComponent, NavbarComponent],
   exports: [RouterModule],
   bootstrap: [TranslatorComponent]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppRoutingModule } from './app/app-routing.module';
 
-bootstrapApplication(AppRoutingModule)
-  .catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppRoutingModule).catch(err => console.error(err));
